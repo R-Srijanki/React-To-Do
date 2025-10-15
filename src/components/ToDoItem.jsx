0b90e3338f9e9cb779c48fa39737d213caa10bb0
R-Srijanki <jankirathod999@gmail.com>
2025-10-15 18:55:14 +0530

    adding comments in components

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -2,20 +2,20 @@ import { useState } from "react";
 import './style.css'
 
 export default function ToDoItem({task,onEdit,index,onDel}){
-    const [complete,setcomplete]=useState(false);
-    function handleedit(){
+    const [complete,setcomplete]=useState(false); //state for complete of task
+    function handleedit(){ //to handle edit of task
         onEdit(index);
     }
-    function handledelete(){
+    function handledelete(){ //to handle delete of task
         onDel(index);
     }
-    function handlecomplete(){
+    function handlecomplete(){ //to change state of complete on click of it
         setcomplete(!complete);
     }
     return(
         <div className="taskstyle">
-            <p className="taskname">{task}</p>
-            <div className="buttons">
+            <p className="taskname">{task}</p> 
+            <div className="buttons"> {/* buttons for edit, delete, complete for each task*/}
             <button onClick={handleedit} className="editbut">Edit</button>
             <button onClick={handledelete} className="delbut">Delete</button>
             <button onClick={handlecomplete} className="combut" style={complete?{backgroundColor:"green"}:{backgroundColor:"gray"}}>{complete?"Completed":"NotComplete"}</button>
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -4,6 +4,7 @@ import './style.css'
 export default function ToDoList({list,onEdit,onDel}){
    
     return(
+        //to map each task in list and send it to ToDoItem component as props .
         <div className="container">
             {
                 list.map((item,index)=><ToDoItem key={index} task={item} onEdit={onEdit} index={index} onDel={onDel}/>)
